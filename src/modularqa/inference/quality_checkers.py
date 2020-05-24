05f0ea3de2f8a3198938e30c9ee813950159fdcf
Tushar Khot <tushark@allenai.org>
2020-05-24 14:23:21 -0700

    dump all final chains

--- src/modularqa/inference/quality_checkers.py
+++ src/modularqa/inference/quality_checkers.py
@@ -1,14 +1,9 @@
 from copy import deepcopy
 
-import torch
-from torch.nn.functional import softmax
-
 from modularqa.inference.model_search import ParticipantModel
 from modularqa.utils.classifier import LMClassifier
 from modularqa.utils.seq_utils import get_sequence_representation
 from modularqa.utils.seq_utils import score_question_answer_chain
-from transformers import InputExample, \
-    glue_convert_examples_to_features, AutoConfig, AutoTokenizer, AutoModelForSequenceClassification
 
 
 class QualityCheckerExample(ParticipantModel):
@@ -44,6 +39,10 @@ class QualityCheckerExample(ParticipantModel):
 
 class ChainOverlapScorer(ParticipantModel):
 
+    def __init__(self, dump_chains=None, **kwargs):
+        self.dump_chains = dump_chains
+        super(ChainOverlapScorer, self).__init__(**kwargs)
+
     def query(self, state, debug=False):
         ## state data
         data = state.data
@@ -53,6 +52,7 @@ class ChainOverlapScorer(ParticipantModel):
         origq = data["query"]
         qchain = data["question_seq"]
         achain = data["answer_seq"]
+        mchain = data["model_seq"]
 
         if debug:
             print("<QUALITYCHECK>: Qs: {} As: {} Q: {}".format(
@@ -63,6 +63,12 @@ class ChainOverlapScorer(ParticipantModel):
             new_tok_score, missed_tok_score, new_toks, missed_toks, unmatched_answers = \
                 score_question_answer_chain(new_qchain, achain, origq, repeat_ok=False,
                                             score_answers=True)
+            if self.dump_chains:
+                sequence = get_sequence_representation(origq, qchain, achain, mchain,
+                                                       for_generation=False)
+                with open(self.dump_chains, 'a') as chains_fp:
+                    chains_fp.write(data["qid"] + "\t" + sequence + "\t" + achain[-1] + "\t" +
+                                    "  ".join(data.get("para_seq")) + "\n")
             if unmatched_answers > 0:
                 if debug:
                     print("Unmatched answers! Rejecting!"
@@ -84,6 +90,9 @@ class ChainOverlapScorer(ParticipantModel):
 
 class LMQualityChecker(LMClassifier, ParticipantModel):
 
+    def __init__(self, dump_chains=None, **kwargs):
+        self.dump_chains = dump_chains
+        super(LMQualityChecker, self).__init__(**kwargs)
 
     def query(self, state, debug=False):
         ## state data
@@ -101,6 +110,8 @@ class LMQualityChecker(LMClassifier, ParticipantModel):
             print("<QUALITYCHECK>: Qs: {} As: {} Q: {}".format(
                 ", ".join(qchain), ", ".join(achain), origq))
 
+        sequence = get_sequence_representation(origq, qchain, achain, mchain, for_generation=False)
+
         # check for correct usage of answers
         if qchain[-1] == "[EOQ]":
             new_qchain = deepcopy(qchain)
@@ -108,6 +119,11 @@ class LMQualityChecker(LMClassifier, ParticipantModel):
             new_tok_score, missed_tok_score, new_toks, missed_toks, unmatched_answers = \
                 score_question_answer_chain(new_qchain, achain, origq, repeat_ok=False,
                                             score_answers=True)
+            if self.dump_chains:
+                with open(self.dump_chains, 'a') as chains_fp:
+                    chains_fp.write(data["qid"] + "\t" + sequence + "\t" + achain[-1] + "\t" +
+                                    "  ".join(data.get("para_seq")) + "\n")
+
             if unmatched_answers > 0:
                 if debug:
                     print("Unmatched answers! Rejecting!"
@@ -116,10 +132,9 @@ class LMQualityChecker(LMClassifier, ParticipantModel):
                 new_state.last_output = "Rejected for bad answers"
                 return new_state
 
-        sequence = get_sequence_representation(origq, qchain, achain, mchain, for_generation=False)
         output_probs = self.score_sequence(sequence1=sequence)
 
-        #print(sequence, output_probs)
-        new_state._score += output_probs[0] # higher is worse; so take prob of 0
+        # print(sequence, output_probs)
+        new_state._score += output_probs[0]  # higher is worse; so take prob of 0
         new_state.last_output = "Score: {}".format(output_probs)
         return new_state

880752943d96ac0908999e101387747566cde5a5
Tushar Khot <tushark@allenai.org>
2020-05-25 18:44:38 -0700

    drop space

--- src/modularqa/inference/configurable_inference_checkpoints.sh
+++ src/modularqa/inference/configurable_inference_checkpoints.sh
@@ -2,7 +2,7 @@
 
 for ckpt_dir in ${MODEL_DIR}/checkpoint-*; do
     MOD_DIR=${ckpt_dir} envsubst < ${CONFIG_FILE} > ${OUTPUT}/new_config.json
-    ckpt_num= ${ckpt_dir#*-}
+    ckpt_num=${ckpt_dir#*-}
     python -m modularqa.inference.configurable_inference \
         --input ${INPUT}/${FILE} \
         --output ${OUTPUT}/ckpt_${ckpt_num}_predictions_{FILE} \

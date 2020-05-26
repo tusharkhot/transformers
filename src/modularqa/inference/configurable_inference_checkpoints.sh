bdac54abb36293ba142b1934ab9014b60885efc5
Tushar Khot <tushark@allenai.org>
2020-05-25 18:00:22 -0700

    add script to evaluate checkpoints

---
+++ src/modularqa/inference/configurable_inference_checkpoints.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for ckpt_dir in ${MODEL_DIR}/checkpoint-*; do
+    MOD_DIR=${ckpt_dir} envsubst < ${CONFIG_FILE} > ${OUTPUT}/new_config.json
+    ckpt_num= ${ckpt_dir#*-}
+    python -m modularqa.inference.configurable_inference \
+        --input ${INPUT}/${FILE} \
+        --output ${OUTPUT}/ckpt_${ckpt_num}_predictions_{FILE} \
+        --config ${OUTPUT}/new_config.json --reader ${DATASET_TYPE}
+done

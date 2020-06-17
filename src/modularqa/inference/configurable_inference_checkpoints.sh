6ce4b3ffd44d211cd7604b5a930d2db452007b47
Tushar Khot <tushark@allenai.org>
2020-06-17 10:42:59 -0700

    also evaluate the final model

--- src/modularqa/inference/configurable_inference_checkpoints.sh
+++ src/modularqa/inference/configurable_inference_checkpoints.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+MOD_DIR=${MODEL_DIR} envsubst < ${CONFIG_FILE} > ${OUTPUT}/new_config.json
+
+python -m modularqa.inference.configurable_inference \
+        --input ${INPUT}/${FILE} \
+        --output ${OUTPUT}/final_predictions_${FILE} \
+        --config ${OUTPUT}/new_config.json --reader ${DATASET_TYPE}
+
 for ckpt_dir in ${MODEL_DIR}/checkpoint-*; do
     MOD_DIR=${ckpt_dir} envsubst < ${CONFIG_FILE} > ${OUTPUT}/new_config.json
     ckpt_num=${ckpt_dir#*-}

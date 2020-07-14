ef73420409eb4605a0cd41ce6e6b59ff4930e87f
Tushar Khot <tushark@allenai.org>
2020-07-14 15:09:11 -0700

    correct colors

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -11,5 +11,5 @@ IMAGE=$IMAGE_NAME_$USER-$GIT_HASH
 
 docker build -f $DOCKERFILE_NAME -t $IMAGE .
 
-echo "\033[0;32m Built image $IMAGE. Now run: \033[0m"
-echo "\033[0;35m beaker image create --name=$IMAGE --desc=\"Transformers:ModularQA Repo; Git Hash: $GIT_HASH\" $IMAGE \033[0m"
+echo -e "\033[0;32m Built image $IMAGE. Now run: \033[0m"
+echo -e "\033[0;35m beaker image create --name=$IMAGE --desc=\"Transformers:ModularQA Repo; Git Hash: $GIT_HASH\" $IMAGE \033[0m"
